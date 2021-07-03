d1970478430ea03d1a0dd2daf442965487ab4656
vasundhrasingh25 <vasu250300@gmail.com>
2021-07-03 20:05:00 +0530

    Update App.js

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import Sidebar from './Sidebar.css';
+import Chat from './Chat.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> CheetChat</h1>
+   
+   <div className="app_body"></div>
+    <SideBar />
+   <Chat />
+   
+    </div>
+  );
+}
+
+
+export default App;
+
