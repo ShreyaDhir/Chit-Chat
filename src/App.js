3fc9f28897ade9fdf6306428068a01613ff500bc
singhhshreya15 <83465477+singhhshreya15@users.noreply.github.com>
2021-07-03 18:36:52 +0530

    app changes
    app.css app.js changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1> ChitChat</h1>
+      <h1> CheetChat</h1>
    
    <div className="app_body"></div>
     {/*SideBar*/}
